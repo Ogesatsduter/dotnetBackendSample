564407d747289366fa3716815094ae23815f13b0
Oge Atsduter <oge.atsduter@gmail.com>
2023-03-03 19:11:15 +0100

    added automapper

---
+++ Backend/DTOs/ExampleDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Backend.DTOs;
+
+public class ExampleDto
+{
+    public int Id { get; set; }
+    public int Number { get; set; }
+    public string Name { get; set; } = String.Empty;
+    public DateTime Date { get; set; }
+    public int TemperatureC { get; set; }
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    public string? Summary { get; set; }
+}
---
+++ Backend/Helpers/AutomapperProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using Backend.DTOs;
+using Backend.Persistence.Models;
+
+namespace Backend.Helpers;
+
+public class AutomapperProfile : Profile
+{
+    public AutomapperProfile()
+    {
+        CreateMap<Example, ExampleDto>().ReverseMap();
+    }
+}
